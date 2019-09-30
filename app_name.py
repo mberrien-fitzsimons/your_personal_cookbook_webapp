e24b97144bff2e794ab0e1a28a9ac9d02f35b980
mishaberrien <markishab@gmail.com>
2019-09-30 09:35:22 -0500

    added templates added a number of templates from the flasky app from miguel book to the templates in my folder

--- app_name.py
+++ app_name.py
@@ -1,1 +1,36 @@
 
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+    meal = StringField('What meal would you like to plan for?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        meal = form.meal.data).first()
+        session['name'] = form.meal.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, meal=session.get('meal'))
