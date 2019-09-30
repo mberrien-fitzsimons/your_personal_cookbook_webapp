b189c4e2cd4cfe2a01b12cc802493e8659c93e8f
mishaberrien <markishab@gmail.com>
2019-09-30 10:36:22 -0500

    added validation error for dietary restrictions

--- app_name.py
+++ app_name.py
@@ -3,16 +3,24 @@ import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, Form, BooleanField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 
-class NameForm(FlaskForm):
+class cartForm(FlaskForm):
     meal = StringField('What meal would you like to plan for?', validators=[DataRequired()])
+    dietrest = (StringField('Omnivore or vegetarian', validators=[DataRequired()]))
+
+    def validate_dietrest(form, field):
+        diet_rest = ['omnivore', 'vegetarian']
+        if field.data.lower() not in diet_rest:
+            raise ValidationError('Must enter either omnivore or vegetarian')
+
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -25,8 +33,11 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = cartForm()
     if form.validate_on_submit():
         session['meal'] = form.meal.data
+        session['dietary_restrictions'] = form.dietrest.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, meal=session.get('meal'))
+    return render_template('index.html',
+                            form=form, meal=session.get('meal'),
+                            dietary_restrictions=session.get('dietary_restrictions'))
