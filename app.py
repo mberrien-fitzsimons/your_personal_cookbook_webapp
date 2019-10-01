26c6a86a42623ac0d7c23506e653b8d19e5fedaa
mishaberrien <markishab@gmail.com>
2019-10-01 09:37:25 -0500

    restaged documents

---
+++ app.py
@@ -0,0 +1,55 @@
+
+import os
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, Form, BooleanField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from models import *
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+class cartForm(FlaskForm):
+    meal = StringField('What meal would you like to plan for?', validators=[DataRequired()])
+    # dietrest = StringField('Omnivore or vegetarian?', validators=[DataRequired()])
+    item1 = StringField('First item', validators=[DataRequired()])
+    item2 = StringField('Second item', validators=[DataRequired()])
+    item3 = StringField('Third item', validators=[DataRequired()])
+
+    # def validate_dietrest(form, field):
+    #     diet_rest = ['omnivore', 'vegetarian']
+    #     if field.data.lower() not in diet_rest:
+    #         raise ValidationError('Must enter either omnivore or vegetarian')
+
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = cartForm()
+    if form.validate_on_submit():
+        meal = form.meal.data
+        # dietary_restrictions = form.dietrest.data
+        item1 = form.item1.data
+        item2 = form.item2.data
+        item3 = form.item3.data
+
+        user_instance = recipeRecommender([item1, item2, item3], meal)
+        user_instance.similair_food_items()
+
+        random_recipes, random_links = user_instance.recipe_recommendations()
+        return render_template('recommendations.html',
+                               recipes=random_recipes,
+                               links=random_links)
+    return render_template('index.html', form=form)
---
+++ models.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+import pickle
+import collections
+import random
+from collections import Counter
+
+infile = open('./crf_ingred_dict','rb')
+recipe_dict = pickle.load(infile)
+infile.close()
+
+infile = open('./crf_tag_dict','rb')
+recipe_tags_dict = pickle.load(infile)
+infile.close()
+
+infile = open('./crf_ingred_dict','rb')
+recipe_dict = pickle.load(infile)
+infile.close()
+
+infile = open('./crf_links_dict','rb')
+recipe_links_dict = pickle.load(infile)
+infile.close()
+
+with open('data_matrix', 'rb') as f:
+    data_matrix = pickle.load(f)
+
+database_food_list = list(data_matrix.columns)
+
+class recipeRecommender(object):
+
+    def __init__(self, shopping_list, meal):
+        self.shopping_list = shopping_list
+        self.meal = meal.title()
+
+    def similair_food_items(self):
+        user_food_inputs = []
+        for item in self.shopping_list:
+            if item in database_food_list:
+                user_food_inputs.append(item)
+        # Construct a new dataframe with the 10 closest neighbours (most similar)
+        # create empty dataframe with correct column names
+        data_neighbours = pd.DataFrame(index=data_matrix.columns, columns=range(1,11))
+        # build matrix with top 10 similair items
+        for i in range(0, len(data_matrix.columns)):
+            data_neighbours.ix[i,:10] = data_matrix.ix[0:,i].sort_values(ascending=False)[:10].index
+
+        # Construct the neighbourhood from the most similar items to the ones our user has already liked.
+        most_similar_to_likes = data_neighbours.loc[user_food_inputs]
+        similar_list = most_similar_to_likes.values.tolist()
+        self.similar_list = [item for sublist in similar_list for item in sublist]
+
+        return self.similar_list
+
+    def recipe_recommendations(self):
+
+        listofkeys = []
+        recipes = []
+        recipe_final = []
+        listofitems = recipe_dict.items()
+
+        for item in listofitems:
+            self.ingred_list = self.extract_ingredients(item[1])
+            recipe_title = item[0]
+            recipes.append(self.ingredient_recipe_matcher(self.ingred_list, self.similar_list, recipe_title))
+        filtered_recipes = filter(None, recipes)
+
+        for lst in filtered_recipes:
+            for item in lst:
+                recipe_final.append(item)
+
+        restricted_recipes = []
+        for title in recipe_final:
+            if self.meal in recipe_tags_dict[title]:
+                restricted_recipes.append(title)
+
+        if len(restricted_recipes) > 5:
+            random_recipes = random.choices(restricted_recipes, k=5)
+        else:
+            random_recipes = restricted_recipes
+
+        links=[]
+        for title in random_recipes:
+            links.append(recipe_links_dict[title])
+
+        return random_recipes, links
+
+    @staticmethod
+    def extract_ingredients(ing_list):
+        ingredients_list = []
+        for ingredient in ing_list:
+            ingredients_list.append(ingredient['name'])
+        return ingredients_list
+
+    @staticmethod
+    def ingredient_recipe_matcher(ingredients_list, similar_list, recipe_title):
+        matching_recipes = []
+        matching_ingredients = []
+        return_recipes = []
+
+        for element in similar_list:
+            if any(element in s for s in ingredients_list):
+                matching_recipes.append(recipe_title)
+        count_recipe_dict = dict(Counter(matching_recipes))
+
+        for key, val in count_recipe_dict.items():
+            return_recipes.append(key)
+
+        return return_recipes
